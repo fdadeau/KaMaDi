acdd348cc5da9e86c32f890cf37aa63ef2ef9495
fdadeau <frederic.dadeau@univ-fcomte.fr>
2016-01-29 23:15:23 +0100

    ajout gamerules

---
+++ js/GameRules.js
@@ -0,0 +1,14 @@
+/**
+ *  Game rules
+ */
+function GameRules() {
+
+    this.shaman = {
+        loadingTime: { 
+            value: 10000,
+            get: function() { return this.value; } 
+        }
+    }
+
+
+}
