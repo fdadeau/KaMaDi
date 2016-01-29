bbd71e6c658e28da3ba11d2a0736e43bb579fa4d
kao98 <kao98@kaonet-fr.net>
2016-01-29 22:14:19 +0100

    Adding Game class

---
+++ js/Game.js
@@ -0,0 +1,43 @@
+"use strict";
+var Game = (function () {
+    
+    function Game(context) {
+        console.log("Game::()");
+        this.initialized = false;
+        this.context = context;
+    }
+    
+    Game.prototype.init = function () {
+        
+        console.log("Game::init");
+        
+        this.initialized = true;
+
+        return this;
+    };
+    
+    Game.prototype.start = function () {
+        
+        console.log("Game::start");
+        
+        if (!this.initialized) {
+            throw new Error("The class Game has not initialized yet");
+        }
+        
+        return this;
+        
+    };
+    
+    Game.prototype.update = function (mousePosition) {
+        if (mousePosition.x !== null) {
+            console.log(mousePosition);
+            mousePosition.raz();
+        }
+    };
+    
+    Game.prototype.render = function () {
+        
+    };
+    
+    return Game;
+})();
--- js/main.js
+++ js/main.js
@@ -1,67 +1,80 @@
 /**
  *  
  */
+
+(function () {
+
+    var CVS_HEIGHT;
+    var CVS_WIDTH;
+    var cvs;
+    var ctx;
     
-var CVS_HEIGHT;
-var CVS_WIDTH;
-var cvs;
-var ctx; 
-
-
-init = function() {
-
-    cvs = document.getElementById("cvs");
-    cvs.onclick = captureMouseClick;
-    ctx = cvs.getContext("2d");
-    CVS_HEIGHT = cvs.height = window.innerHeight - 10;
-    CVS_WIDTH = cvs.width = window.innerWidth - 10;
-
+    var mouse_position = {
+        x: null,
+        y: null,
+        raz: function () { this.x = null && this.y = null }
+    };
     
-    boucleDeJeu();
-}
+    var game;
 
-window.requestAnimFrame = (function(){
-  return  window.requestAnimationFrame       ||
-          window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame    ||
-          function( callback ){
-            window.setTimeout(callback, 1000 / 60);
-          };
+    init = function() {
+
+        cvs = document.getElementById("cvs");
+        cvs.onclick = captureMouseClick;
+        ctx = cvs.getContext("2d");
+        CVS_HEIGHT = cvs.height = window.innerHeight - 10;
+        CVS_WIDTH = cvs.width = window.innerWidth - 10;
+
+        game = new Game(ctx);
+        game.init();
+
+        boucleDeJeu();
+    };
+
+    window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       ||
+              window.webkitRequestAnimationFrame ||
+              window.mozRequestAnimationFrame    ||
+              function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+              };
+    })();
+
+
+    boucleDeJeu = function() {
+        currentDate = Date.now();
+        requestAnimFrame(boucleDeJeu);
+        update(currentDate);
+        render();
+    };
+
+
+    update = function(d) { 
+        game.update(d, mouse_position);
+    };
+
+    render = function() {
+        ctx.clearRect(0, 0, CVS_WIDTH, CVS_HEIGHT);
+        game.render();
+    };
+
+    captureMouseClick = function(event) {
+        
+        // gets the click coordinates
+        if (event.x != undefined && event.y != undefined) {
+                x = event.x;
+                y = event.y;
+        }	
+        else { // Firefox method to get the position
+                x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+                y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+        }
+        x -= cvs.offsetLeft; 
+        y -= cvs.offsetTop;
+
+        mouse_position.x = x;
+        mouse_position.y = y;
+
+    };
+
 })();
-
-    
-boucleDeJeu = function() {
-    currentDate = Date.now();
-    requestAnimFrame(boucleDeJeu);
-    update(currentDate);
-    render();
-}   
-
-
-update = function(d) { 
-
-}
-
-render = function() {
-    ctx.clearRect(0, 0, CVS_WIDTH, CVS_HEIGHT);
-
-}
-
-captureMouseClick = function(event) {
-    // gets the click coordinates
-	if (event.x != undefined && event.y != undefined) {
-		x = event.x;
-		y = event.y;
-	}	
-	else { // Firefox method to get the position
-		x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-		y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-	}
-	x -= cvs.offsetLeft; 
-	y -= cvs.offsetTop;
-	 
-    alert("click on (" + x + ","+ y + ")");
-    
-    // TODO
-}
-    
