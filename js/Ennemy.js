277f59200078f6a025e87c6730d94fc2aebf46d8
fdadeau <frederic.dadeau@univ-fcomte.fr>
2016-01-30 00:25:46 +0100

    ajout ennemis

---
+++ js/Ennemy.js
@@ -0,0 +1,30 @@
+function Ennemy(_game, _x, _y) {
+
+    this.game = _game;
+    
+    // position
+    this.x = _x;
+    this.y = _y;
+    
+    // size
+    this.width = 20;
+    this.height = 20;
+    
+    this.life = game.gameRules.ennemies.life.get();
+
+}
+
+
+Ennemy.prototype.update = function(time) {
+    
+    this.x--;
+        
+}
+
+
+Ennemy.prototype.render = function() {
+    
+    this.game.context.fillStyle = "#FF0000";
+    this.game.context.fillRect(this.x-this.width/2, this.y-this.height/2, this.width, this.height);
+    
+}
